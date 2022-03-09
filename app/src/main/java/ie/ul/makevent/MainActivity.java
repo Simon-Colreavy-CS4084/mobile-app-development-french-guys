24575f7e374a374691d2e7663214a4d04b800638
elendir41 <81863269+elendir41@users.noreply.github.com>
2022-03-09 19:48:20 +0000

    Adding the tabs

--- app/src/main/java/ie/ul/makevent/MainActivity.java
+++ app/src/main/java/ie/ul/makevent/MainActivity.java
@@ -1,14 +1,40 @@
 package ie.ul.makevent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Profile(), "Profile");
+        vpAdapter.addFragment(new Messages(), "Messages");
+        vpAdapter.addFragment(new Public(), "Public");
+        vpAdapter.addFragment(new Private(), "Private");
+        vpAdapter.addFragment(new Parametres(), "Parametres");
+
+        viewPager.setAdapter(vpAdapter);
+
     }
+
+
 }
