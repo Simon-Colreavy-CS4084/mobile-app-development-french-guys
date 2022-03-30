83eb793907f8b4127e7334f4413b98d9b096d8ad
Antony Phonesavanh <antony.phonesavanh@epita.fr>
2022-03-30 17:52:18 +0100

    Button back

--- app/src/main/java/ie/ul/makevent/Private.java
+++ app/src/main/java/ie/ul/makevent/Private.java
@@ -1,6 +1,7 @@
 package ie.ul.makevent;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,10 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import ie.ul.makevent.activities.ChatActivity;
 import ie.ul.makevent.utilities.Constants;
+
 
 
 public class Private extends Fragment  {
@@ -24,9 +28,7 @@ public class Private extends Fragment  {
                              Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.fragment_events , container, false);
-
-        Button buttonEvents = view.findViewById(R.id.buttonEvents);
-        buttonEvents.setOnClickListener(v -> onClickButton());
+        view.findViewById(R.id.buttonEvents).setOnClickListener(v -> onClickButton());
         // Inflate the layout for this fragment
         return view;
     }
--- app/src/main/java/ie/ul/makevent/Public.java
+++ app/src/main/java/ie/ul/makevent/Public.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Public extends AppCompatActivity {
+import ie.ul.makevent.databinding.ActivityPublicBinding;
 
+public class Public extends AppCompatActivity {
+    private ActivityPublicBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityPublicBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_public);
+        binding.imageBackEvents.setOnClickListener(v -> onBackPressed());
     }
 }
